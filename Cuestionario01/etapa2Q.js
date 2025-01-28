50e6eece58b0c159c59ccec478b13442a3a4dc24
megadatogit <megadato82@gmail.com>
2025-01-28 17:26:23 -0600

    cuestionario despliegue

--- Cuestionario01/etapa2Q.js
+++ Cuestionario01/etapa2Q.js
@@ -1,4 +1,20 @@
-const cntRspuesta = document.querySelectorAll('.respuesta');
-let respuesta = document.querySelector([])
+document.addEventListener("DOMContentLoaded", (event) => {  
+    const consumeSi = document.getElementById("consumeSi");  
+    const consumeNo = document.getElementById("consumeNo");  
+    const cntConsume = document.getElementById("cntConsume");  
 
+    // Inicialmente ocultar el contenedor  
+    cntConsume.style.display = "none";  
 
+    consumeSi.addEventListener("change", (event) => {  
+        if (consumeSi.checked) {  
+            cntConsume.style.display= "block";  
+        }  
+    });  
+
+    consumeNo.addEventListener("change", (event) => {  
+        if (consumeNo.checked) {  
+            cntConsume.style.display = "none";  
+        }  
+    });  
+});
